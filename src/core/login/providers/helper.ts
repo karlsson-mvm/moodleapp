49702f40a6fcd52466bba6bb8a6e9d2d038bdec1
Riccardo D'Angelo <riccardo@edisonweb.com>
2020-04-06 09:51:40 +0200

    First bundle narbone

--- src/core/login/providers/helper.ts
+++ src/core/login/providers/helper.ts
@@ -1205,7 +1205,7 @@ export class CoreLoginHelperProvider {
         } else if (this.appProvider.isMac()) {
             link = 'itms-apps://itunes.apple.com/app/id1255924440';
         } else if (this.platform.is('android')) {
-            link = 'market://details?id=com.scuolainrete.narbone';
+            link = 'market://details?id=com.moodle.moodlemobile';
         } else if (this.platform.is('ios')) {
             link = 'itms-apps://itunes.apple.com/app/id633359593';
         }
